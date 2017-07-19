3def5e0c290b9ccad85d952bc35724973ba6ccdf
kumada626 <kumada626@users.noreply.github.com>
2017-07-20 06:51:55 +0900

    fix: fix sd_step argument (#101)

--- deploy.sh
+++ deploy.sh
@@ -3,12 +3,12 @@
 source ./ci/git-ssh.sh
 mkdir _deploy
 cd _deploy
-sd-step exec git 'git clone -b gh-pages git@github.com:screwdriver-cd/guide.git'
+sd-step exec core/git 'git clone -b gh-pages git@github.com:screwdriver-cd/guide.git'
 cd guide
 rm -rf *
 cp -R ../../_site/* .
-sd-step exec git 'git add -A'
-sd-step exec git 'git commit -m "Deployed by Screwdriver"'
-sd-step exec git 'git push origin gh-pages'
+sd-step exec core/git 'git add -A'
+sd-step exec core/git 'git commit -m "Deployed by Screwdriver"'
+sd-step exec core/git 'git push origin gh-pages'
 cd ../..
 rm -rf _deploy
